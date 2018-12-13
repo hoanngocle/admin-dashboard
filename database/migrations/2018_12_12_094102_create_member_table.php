d6e2470c0f58a36c86b15511f9a3e5f779fb21a1
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2018-12-13 10:53:18 +0700

    V1.0 - Add table Admin + Member

--- database/migrations/2018_12_12_094102_create_member_table.php
+++ database/migrations/2018_12_12_094102_create_member_table.php
@@ -20,21 +20,22 @@ class CreateMemberTable extends Migration
             $table->string('username', 255)->unique();
             $table->string('password', 255);
             $table->timestamp('password_expried_time');
-            $table->timestamp('log_date')->comment('Show last login time of admin');
+            $table->timestamp('log_date')->nullable()->comment('Show last login time of admin');
             $table->integer('log_num')->unsigned()->default(1)->comment('Count login times of admin');
             $table->tinyInteger('is_online')->unsigned()->default(1)
                 ->comment('Member is online or offline: 0 - offline; 1 - online');
             $table->tinyInteger('is_active')->unsigned()->default(1)
                 ->comment('Admin account is active or block: 0 - block; 1 - active');
             $table->tinyInteger('acl_flag')->unsigned()->comment('Admin roles: 1 - mod ; 99 - admin');
-            $table->string('nickname', 20);
-            $table->dateTime('dob')->comment('Date Of Birth');
-            $table->string('gender', 20);
+            $table->string('nickname', 50);
+            $table->dateTime('dob')->nullable()->comment('Date Of Birth');
+            $table->string('gender', 20)->nullable();
             $table->float('coin')->unsigned()->default(0)->comment('Coin of member');
-            $table->unsignedInteger('level')->default(1)->comment('Level of member');
+            $table->unsignedInteger('level')->default(1)->comment('Level of member: define in table tm_level');
             $table->float('exp')->unsigned()->default(0)->comment('Exp of member');
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
+            $table->integer('upuser')->nullable();
         });
     }
 
