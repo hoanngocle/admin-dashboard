e46e5b8a608b15a73c1b87e885be4931b6c6bbb2
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2018-12-14 18:31:22 +0700

    V1.0 Update table

--- database/migrations/2018_12_13_173622_create_loan_debt_table.php
+++ database/migrations/2018_12_13_173622_create_loan_debt_table.php
@@ -18,8 +18,10 @@ class CreateLoanDebtTable extends Migration
             $table->unsignedInteger('member_id')->nullable();
             $table->string('name', 255);
             $table->bigInteger('value')->comment('Value of debt or loan');
+            $table->string('description', 255);
             $table->enum('type', ['Loan', 'Debt'])->default('Loan');
             $table->string('loan_debt_person', 100)->nullable();
+            $table->tinyInteger('is_done')->default(0)->comment('0: Running; 1: Done');
             $table->dateTime('created_at');
             $table->dateTime('updated_at')->nullable();
             $table->softDeletes();
