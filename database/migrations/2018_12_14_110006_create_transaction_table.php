9a8aa19fb4b5858e9afc56a1809505ab66222957
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2018-12-14 11:07:00 +0700

    V1.0 Add budget, saving update transaction, loan debt table

--- database/migrations/2018_12_14_110006_create_transaction_table.php
+++ database/migrations/2018_12_14_110006_create_transaction_table.php
@@ -19,11 +19,15 @@ class CreateTransactionTable extends Migration
             $table->unsignedInteger('category_id')->nullable();
             $table->unsignedInteger('wallet_id')->nullable();
             $table->unsignedInteger('loan_debt_id')->nullable();
+            $table->unsignedInteger('budget_id')->nullable();
+            $table->unsignedInteger('saving_id')->nullable();
             $table->string('name', 100);
             $table->bigInteger('value')->comment('Value of transaction');
             $table->string('description', 255)->comment('Show content of transaction');
             $table->enum('type', ['Inflow', 'Outflow'])->comment('Type of transaction');
-            $table->timestamps();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+            $table->integer('upuser')->nullable();
         });
 
         Schema::table('tbl_transaction', function (Blueprint $table) {
@@ -31,6 +35,8 @@ class CreateTransactionTable extends Migration
             $table->foreign('category_id')->references('category_id')->on('tbl_category')->onDelete('SET NULL');
             $table->foreign('wallet_id')->references('wallet_id')->on('tbl_wallet')->onDelete('SET NULL');
             $table->foreign('loan_debt_id')->references('loan_debt_id')->on('tbl_loan_debt')->onDelete('SET NULL');
+            $table->foreign('budget_id')->references('budget_id')->on('tbl_budget')->onDelete('SET NULL');
+            $table->foreign('saving_id')->references('saving_id')->on('tbl_saving')->onDelete('SET NULL');
         });
     }
 
