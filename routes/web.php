1c3c252049e0b2284972fc9141870bcb5942d5d9
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2018-12-17 17:08:58 +0700

    V1.0 - Add Controller + Model + routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('/', 'DashBoardController@index');
+
+Route::prefix('admin')->group(function () {
+    Route::resource('login', 'LoginController@index');
 });
