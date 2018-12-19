3b5a95295b5381327acb25b40cb68bc024fa7e4f
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2018-12-19 10:21:11 +0700

    V1.0 - Add forgot password page

---
+++ resources/views/admin/auth/forgot-password.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div class="login-content">
+        <form action="index.html" method="GET" class="margin-bottom-0">
+            <div class="form-group m-b-20">
+                <input type="text" class="form-control form-control-lg" placeholder="Email"/>
+            </div>
+            <div class="login-buttons">
+                <button type="submit" class="btn btn-success btn-block btn-lg">Sending Token</button>
+            </div>
+            <div class="m-t-20">
+                Already a member? Click <a href="{{ url('admin/login') }}">here</a> to login.
+            </div>
+        </form>
+    </div>
+@endsection
