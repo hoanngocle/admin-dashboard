2410f1981318fbe5317929af968abe32f866bae1
Hoàn Ngọc Lê <hoanln6636@co-well.com.vn>
2019-01-10 17:40:14 +0700

    V1.0 - Dashboard - test contribute

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -23,6 +23,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        // test contribute
         $nickName   = $this->userFactory->getUsername();
         $avatar     = $this->userFactory->getAvatar();
 
